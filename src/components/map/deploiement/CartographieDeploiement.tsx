b79ac91999ec13b8f8f80417973507b282968f70
Julien Demoutiez <julien.demoutiez@gmail.com>
2025-09-15 20:02:06 +0200

    ✨(map) fix lint issues

--- src/components/map/deploiement/CartographieDeploiement.tsx
+++ src/components/map/deploiement/CartographieDeploiement.tsx
@@ -1,77 +1,74 @@
 "use client";
 
-import { useCallback, useEffect, useMemo, useState } from "react";
-import SidePanelContent from "./SidePanelContent";
+import { useCallback, useMemo } from "react";
 import MapWrapper from "../MapWrapper";
-import { ReferentielConformite } from "@/pages/conformite/referentiel";
+import SidePanelContent from "./SidePanelContent";
+// import { ReferentielConformite } from "@/pages/conformite/referentiel";
 
 import {
-  AllStats,
+  // AllStats,
   AreaStats,
-  SelectedArea,
-  StatRecord,
 } from "../types";
 
 const CartographieDeploiement = () => {
-  const [stats, setStats] = useState<AllStats>({} as AllStats);
+  // const [stats, setStats] = useState<AllStats>({} as AllStats);
 
-  const statsParams = useMemo(() => ({
-  }), []);
+  const statsParams = useMemo(() => ({}), []);
 
   // const loadStats = useCallback(
-    // async (level: "region" | "department" | "epci"): Promise<Record<string, StatRecord[]>> => {
-    //   const scope = {
-    //     region: "reg",
-    //     department: "dep",
-    //     epci: "epci",
-    //   };
-    //   const response = await fetch(
-    //     `/api/rcpnt/stats?scope=${scope[level]}&refs=${allRcpntRefs.join(",")}`,
-    //     {
-    //       method: "GET",
-    //       headers: { "Content-Type": "application/json" },
-    //       next: { revalidate: 3600 },
-    //     },
-    //   );
-    //   const data = await response.json();
-    //   return data;
-    // },
-    // [allRcpntRefs],
+  // async (level: "region" | "department" | "epci"): Promise<Record<string, StatRecord[]>> => {
+  //   const scope = {
+  //     region: "reg",
+  //     department: "dep",
+  //     epci: "epci",
+  //   };
+  //   const response = await fetch(
+  //     `/api/rcpnt/stats?scope=${scope[level]}&refs=${allRcpntRefs.join(",")}`,
+  //     {
+  //       method: "GET",
+  //       headers: { "Content-Type": "application/json" },
+  //       next: { revalidate: 3600 },
+  //     },
+  //   );
+  //   const data = await response.json();
+  //   return data;
+  // },
+  // [allRcpntRefs],
   // );
 
   // const loadAllStats = useCallback(async () => {
-    // const regionStats = await loadStats("region");
-    // const departmentStats = await loadStats("department");
-    // const epciStats = await loadStats("epci");
-    // let countryStats;
-    // try {
-    //   countryStats = {
-    //     "00": allRcpntRefs.map((ref) => {
-    //       return {
-    //         ref: ref,
-    //         valid: Object.values(regionStats).reduce(
-    //           (acc, stat) => acc + (stat.find((s) => s.ref === ref)?.valid || 0),
-    //           0,
-    //         ),
-    //         total: Object.values(regionStats).reduce(
-    //           (acc, stat) => acc + (stat.find((s) => s.ref === ref)?.total || 0),
-    //           0,
-    //         ),
-    //       };
-    //     }),
-    //   };
-    // } catch {
-    //   countryStats = {
-    //     "00": [],
-    //   };
-    // }
+  // const regionStats = await loadStats("region");
+  // const departmentStats = await loadStats("department");
+  // const epciStats = await loadStats("epci");
+  // let countryStats;
+  // try {
+  //   countryStats = {
+  //     "00": allRcpntRefs.map((ref) => {
+  //       return {
+  //         ref: ref,
+  //         valid: Object.values(regionStats).reduce(
+  //           (acc, stat) => acc + (stat.find((s) => s.ref === ref)?.valid || 0),
+  //           0,
+  //         ),
+  //         total: Object.values(regionStats).reduce(
+  //           (acc, stat) => acc + (stat.find((s) => s.ref === ref)?.total || 0),
+  //           0,
+  //         ),
+  //       };
+  //     }),
+  //   };
+  // } catch {
+  //   countryStats = {
+  //     "00": [],
+  //   };
+  // }
 
-    // setStats({
-    //   region: regionStats,
-    //   department: departmentStats,
-    //   epci: epciStats,
-    //   country: countryStats,
-    // });
+  // setStats({
+  //   region: regionStats,
+  //   department: departmentStats,
+  //   epci: epciStats,
+  //   country: countryStats,
+  // });
   // }, [allRcpntRefs, loadStats]);
 
   const computeAreaStats = useCallback(
@@ -85,7 +82,7 @@ const CartographieDeploiement = () => {
         if (level === "city") {
           return {
             score: Math.random() * 100,
-          }
+          };
         } else {
           return {
             n_cities: Math.random() * 100,
