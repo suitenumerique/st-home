50c31820007f312256e9eb9183d4e9b198f3ff1e
juliendemoutiez <julien.demoutiez@gmail.com>
2026-04-23 16:05:26 +0200

    ✨ (map) split lsp deploy map and incub deploy map

--- src/pages/carto-deploy-incub-embed.tsx
+++ src/pages/carto-deploy-incub-embed.tsx
@@ -6,24 +6,16 @@ import styles from "../styles/cartographie-deploiement.module.css";
 export default function CartoDeployIncubEmbedPage() {
   useEffect(() => {
     const footer = document.querySelector<HTMLElement>(".fr-footer");
-    if (footer) {
-      footer.style.display = "none";
-    }
-
+    const header = document.querySelector<HTMLElement>(".fr-header");
+    if (footer) footer.style.display = "none";
+    if (header) header.style.display = "none";
     document.body.style.overflow = "hidden";
-
-    return () => {
-      if (footer) {
-        footer.style.display = "";
-      }
-      document.body.style.overflow = "";
-    };
   }, []);
 
   return (
     <>
       <NextSeo noindex={true} nofollow={true} />
-      <div className={styles.mapContainer}>
+      <div className={styles.mapContainerEmbed}>
         <CartographieDeploiement isLSTMode={false} />
       </div>
     </>
