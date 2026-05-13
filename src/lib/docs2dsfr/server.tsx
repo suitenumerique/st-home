782b05d5dd32e7e0f1fe03956517eb3d72ed1fef
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2026-05-13 13:05:11 +0200

    🐛(cms) relax frontmatter detection

--- src/lib/docs2dsfr/server.tsx
+++ src/lib/docs2dsfr/server.tsx
@@ -145,7 +145,7 @@ export async function getDocument(
 
   // Extract frontmatter from the document content
   const frontmatter = document.content.match(
-    /^<p>---<\/p>((<p>[a-z0-9_-]+\:\s.+?<\/p>)+)<p>---<\/p>/,
+    /^<p>\s*---\s*(?:<br\s*\/?>\s*)?<\/p>((<p>[a-z0-9_-]+\:\s.+?<\/p>)+)<p>\s*---\s*(?:<br\s*\/?>\s*)?<\/p>/,
   );
   if (frontmatter && frontmatter[1]) {
     document.frontmatter =
